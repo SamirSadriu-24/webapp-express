ea48e8a4c83eeeff5873f1d64eee89ad4dcfc0e2
samir <samirsadriuboolean@gmail.com>
2025-10-30 15:36:35 +0100

    creazione della nuova applicazione Express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Web.app.Movies")
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server Listening on port http://127.0.0.1:${port}`)
+})
