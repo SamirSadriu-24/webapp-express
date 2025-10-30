73bc09ba0e32f78ceca59ddb90abe549d8fe34cf
samir <samirsadriuboolean@gmail.com>
2025-10-30 16:55:03 +0100

    aggiunta commenti per sistemazione futura

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connection = require('../data/db');
 const router = express.Router();
 
+//mostra tutti i post
+
 router.get('/', (req, res) => {
     const sql = "SELECT * FROM movies"
     connection.query(sql, (err, results) => {
@@ -14,6 +16,8 @@ router.get('/', (req, res) => {
     });
 })
 
+
+// mostra il post singolo 
 router.get('/:id', (req,res) => {
     const id= req.params.id
     const sql = "select M.*, JSON_ARRAYAGG(R.text) from movies as M join reviews as R on R.movie_id = M.id where M.id = ?"
