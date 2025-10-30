15e7cca627aca6a9d393b7203e78c52b6eff270b
samir <samirsadriuboolean@gmail.com>
2025-10-30 16:46:35 +0100

    finito show.

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -2,15 +2,28 @@ const express = require('express');
 const connection = require('../data/db');
 const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     const sql = "SELECT * FROM movies"
     connection.query(sql, (err, results) => {
         if (err) {
             console.error("Errore nella query:", err);
+            res.status(500).send("Errore nel database");
             return;
         }
         res.json(results); // risultati della query
     });
 })
+
+router.get('/:id', (req,res) => {
+    const id= req.params.id
+    const sql = "select M.*, JSON_ARRAYAGG(R.text) from movies as M join reviews as R on R.movie_id = M.id where M.id = ?"
+    connection.query(sql, [id], (err, results) => {
+        if (err) {
+            res.status(500).send("errore nel database");
+            return;
+        }
+        res.json(results);
+    })
+});
 
 module.exports = router
