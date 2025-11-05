2a227f68bfbef0b200ec86d98e778f5e0587f2fa
samir <samirsadriuboolean@gmail.com>
2025-11-05 14:54:18 +0100

    creazione della route post per creare recensioni e prova su postman funzionante

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -18,8 +18,8 @@ router.get('/', (req, res) => {
 
 
 // mostra il post singolo 
-router.get('/:id', (req,res) => {
-    const id= req.params.id
+router.get('/:id', (req, res) => {
+    const id = req.params.id
     const sql = "select M.*, JSON_ARRAYAGG(JSON_OBJECT('name', R.name, 'text', R.text, 'vote', R.vote)) as reviews from movies as M join reviews as R on R.movie_id = M.id where M.id = ? group by M.id"
     connection.query(sql, [id], (err, results) => {
         if (err) {
@@ -29,5 +29,24 @@ router.get('/:id', (req,res) => {
         res.json(results);
     })
 });
+
+//postare il nuovo dato
+router.post('/:id', (req, res) => {
+    const movie_id = req.params.id
+    const { name, text, vote } = req.body;
+    const sql = "insert into reviews (movie_id, name, text, vote) VALUES (?, ?, ?, ?) "
+
+    connection.query(sql, [movie_id, name, text, vote], (err, result) => {
+        if (err) {
+            res.status(500).send("Errore, il tuo dato non è stato inviato al database")
+            return;
+        }
+        res.status(201).json({
+            message: "La tua recensione è stata aggiunta!",
+        });
+    })
+
+})
+
 
 module.exports = router
